1e5974f2cbba63c4fc5d5f15fba608846f0e681f
Jérôme Abel <contact@jeromeabel.net>
2022-12-15 17:59:05 +0100

    Tests : click on eye icon should open the modal

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -4,17 +4,21 @@
 
 import {screen, waitFor} from "@testing-library/dom"
 import { toHaveClass } from "@testing-library/jest-dom"
+import userEvent from '@testing-library/user-event'
+
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
+import { ROUTES, ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
 
 import router from "../app/Router.js";
+import Bills from "../containers/Bills.js"
 
 describe("Given I am connected as an employee", () => {
-  describe("When I am on Bills Page", () => {
-    test("Then bill icon in vertical layout should be highlighted", async () => {
 
+  describe("When I am on Bills Page", () => {
+
+    test("Then bill icon in vertical layout should be highlighted", async () => {
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
       window.localStorage.setItem('user', JSON.stringify({
         type: 'Employee'
@@ -27,7 +31,7 @@ describe("Given I am connected as an employee", () => {
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
       
-      //to-do write expect expression
+      // New test
       expect(windowIcon).toHaveClass('active-icon')
     })
 
@@ -36,8 +40,44 @@ describe("Given I am connected as an employee", () => {
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       //const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       //const datesSorted = [...dates].sort( antiChrono)
+      // Debug sort
       const datesSorted = [...dates].sort( (a,b) => b - a )
       expect(dates).toEqual(datesSorted)
     })
+
+    // New tests
+    describe('When I click on the first eye icon', () => {
+      test('Then it should open the modal', () => {
+        //?? quand est-ce qu'on a besoin d'un async await ?
+  
+        $.fn.modal = jest.fn(); // jQuery Mock ?
+  
+        // ...
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname })
+        }
+        // ...
+        Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+        window.localStorage.setItem('user', JSON.stringify({ type: 'Employee' }));
+    
+        document.body.innerHTML = BillsUI({ data: bills });
+  
+        // ?? store mocked
+        const billsContainer = new Bills({
+          document, onNavigate, localStorage: window.localStorage
+        });
+
+        const eyeIcon = screen.getAllByTestId('icon-eye')[0]; // ?? only the first one
+        const openModal = jest.fn(billsContainer.handleClickIconEye(eyeIcon));
+        eyeIcon.addEventListener('click', openModal);
+        userEvent.click(eyeIcon); 
+
+        expect(openModal).toHaveBeenCalled(); 
+        const modal = screen.getByTestId('modalFileEmployee'); // ajouté dans BillsUI
+        expect(modal).toBeTruthy();
+        //expect(modal).toHaveClass("show") // ?? ne marche pas
+      })
+    })
+
   })
 })
